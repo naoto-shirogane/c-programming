281f088e8b2505b51e9e444e25975fe5e3f501fb
naotoshirogane <rafael2409b@gmail.com>
2025-03-24 09:29:52 -0400

    Made some changes to c7-4.c

--- practice/c7/c7-4.c
+++ practice/c7/c7-4.c
@@ -1,3 +1,31 @@
+/* c7-4.c
+
+   Rafael Bonilla
+   March 23, 2025
+  
+   C Programming: A Modern Approach, Second Edition
+   Chapter 7, Programming Project 4
+
+   Write a program that translates an alphabetic phone
+   number into numeric form:
+
+       Enter phone number: CALLATT
+       2255288
+
+   (In case you don't have a telephone nearby, here are
+   the letters on the keys: 2=ABC, 3=DEF, 4=GHI, 5=JKL,
+   6=MNO, 7=PRS, 8=TUV, 9=WXY.) If the original phone
+   number contains nonalphabetic characters (digits or
+   punctuation, for example), leave them unchanged:
+
+       Enter phone number: 1-800-COL-LECT
+       1-800-265-5328
+
+   You may assume that any letters entered by the user
+   are uppercase.
+
+*/
+
 #include <stdio.h>
 #include <ctype.h>
 
